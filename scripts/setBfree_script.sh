746a06efd8847a918a57963819befac7a20cc8bf
jkschwartz <jordan.kyle.schwartz@gmail.com>
2025-09-10 19:22:47 +0100

    updating setBfree start up script

--- scripts/setBfree_script.sh
+++ scripts/setBfree_script.sh
@@ -1,11 +1,6 @@
-setBfree &
+setBfreeUI -j setBfreeJack &
 
 sleep 2 
 
-jack_connect aeolus:out.L system:playback_1 &> /dev/null
-
-jack_connect aeolus:out.R system:playback_2 &> /dev/null
-
-
-jack_connect system:midi_capture_1 aeolus:Midi/in &> /dev/null
+jack_connect system:midi_capture_2 setBfreeJack:control &> /dev/null
 
