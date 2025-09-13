b6dac639595d4729a08307171853250a819ce43a
jkschwartz <jordan.kyle.schwartz@gmail.com>
2025-09-13 03:55:27 +0100

    engine row is starting to wiggle but is just a nice light show

--- keybow/code.py
+++ keybow/code.py
@@ -1,13 +1,5 @@
 import board
 from keybow2040 import Keybow2040
-#from enum import Enum
-
-class States(Enum):
-    HOME = 1
-    SFIZZ = 2
-    AEOLUS = 3
-    SETBFREE = 4
-    PD = 5
 
 # Set up Keybow
 i2c = board.I2C()
@@ -16,14 +8,14 @@ keys = keybow.keys
 
 # Colors
 rgb = (0, 255, 255)
-engine = (0, 255, 255)
-selected_engine(200,240,240)
-non_selected_engine(0,40,40)
-preset = (0, 255, 0)
+engine = (0, 150, 150)
+selected_engine = (100,255,255)
+non_selected_engine = (0,40,40)
+preset = (70, 50, 0)
 settings = (0, 0, 255)
-non_functional = (15,15,15)
+non_functional = (5,5,5)
 engine_keys = [3,7,11,15]
-setting_keys = [8,12]
+setting_keys = [12]
 
 # Keys
 sfizz_key = 3
@@ -46,15 +38,38 @@ def aeolus_view(keys):
     for i in range(16):
         if i == aeolus_key:
             keys[i].set_led(*selected_engine)
-        elif i in engine_key:
+        elif i in setting_keys:
+            keys[i].set_led(*settings)
+        elif i in engine_keys:
+            keys[i].set_led(*non_selected_engine)
+        else:
+            keys[i].set_led(*preset)
+
+
+def setBfree_view(keys):
+    for i in range(16):
+        if i == setBfree_key:
+            keys[i].set_led(*selected_engine)
+        elif i in setting_keys:
+            keys[i].set_led(*settings)
+        elif i in engine_keys:
             keys[i].set_led(*non_selected_engine)
         else:
             keys[i].set_led(*preset)
 
 #default view
-home_view(keys)
-
+current_engine = aeolus_view
+current_engine(keys)
 while True:
     # Always remember to call keybow.update() on every iteration of your loop!
     keybow.update()
-
+    current_engine(keys)
+    for key in keys:
+        if key.pressed:
+            if key.get_number() == aeolus_key:
+                current_engine = aeolus_view
+            elif key.get_number() == setBfree_key:
+                current_engine = setBfree_view
+            elif key.get_number() in engine_keys:
+                current_engine = home_view
+            key.set_led(99,99,94)
